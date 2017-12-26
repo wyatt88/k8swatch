17e3ccb62d077be25a2b363e3542dca8332023d5
wenwen3 <wenwen3@lenovo.com>
2017-12-26 18:35:38 +0800

    update

---
+++ vendor/github.com/wyatt88/k8swatch/README.md
@@ -0,0 +1,2 @@
+# k8swatch
+Watch kubernetes event resource and send the message to Alertmanager
---
+++ vendor/github.com/wyatt88/k8swatch/pkg/event/event.go
@@ -0,0 +1,35 @@
+package event
+
+import (
+	"k8s.io/api/core/v1"
+)
+
+type Event struct {
+	Namespace string
+	Kind      string
+	Reason    string
+	Name      string
+	Message   string
+}
+
+// New create new Kubewatch Event
+func New(obj interface{}) Event {
+	var namespace, kind, reason, name, message string
+	if apiService, ok := obj.(*v1.Event); ok {
+		namespace = apiService.Namespace
+		name = apiService.Name
+		kind = apiService.InvolvedObject.Kind
+		reason = apiService.Reason
+		message = apiService.Message
+	}
+	
+	kbEvent := Event{
+		Namespace: namespace,
+		Kind:      kind,
+		Reason:    reason,
+		Name:      name,
+		Message: message,
+	}
+	
+	return kbEvent
+}
